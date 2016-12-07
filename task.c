b4dfff7758d83705f921b9e631843cca11a4bf53
jongchank <jongchank@me.com>
2016-12-07 11:28:46 +0900

    Fix indentation

--- task.c
+++ task.c
@@ -3,28 +3,26 @@
 #include "serial.h"
 #include "Timer1.h"
 
-extern int led;
-
 /* Timer1 ISR2 */
 ISR2(timer1_handler)
 {
     static unsigned long c = 0;
-	TickType cycle;
-	GetAlarm(counter1, &cycle);
-	CounterTick(counter1);
+    TickType cycle;
+    GetAlarm(counter1, &cycle);
+    CounterTick(counter1);
     printfSerial("\n%4lu: ", ++c);
 }
 
 TASK(TaskL1) {
-	printfSerial("TaskL1 begins. ");
+    printfSerial("TaskL1 begins. ");
     delay(2100);
-	printfSerial("TaskL1 finishes. ");
-	TerminateTask();
+    printfSerial("TaskL1 finishes. ");
+    TerminateTask();
 };
 
 TASK(TaskL2) {
-	printfSerial("TaskL2 begins. ");
+    printfSerial("TaskL2 begins. ");
     delay(2100);
-	printfSerial("TaskL2 finishes. ");
-	TerminateTask();
+    printfSerial("TaskL2 finishes. ");
+    TerminateTask();
 };
